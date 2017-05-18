d637e6d73d5f0dae9b661411e076e0a635435ccf
mkovachev <martin.kovachev@outlook.com>
2017-05-18 18:56:54 +0300

    added profile view

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('*', (req, res) => {
 });
 
 // start server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
